41a001d21d3a646d8a7c2e35f625894cec664cf8
Loïc Ortola <contact@loicortola.com>
2016-05-22 18:05:24 +0200

    Fixed Jenkins security reset issue

--- ci-server/roles/jenkins/templates/basic-security.groovy
+++ ci-server/roles/jenkins/templates/basic-security.groovy
@@ -1,16 +1,18 @@
 #!groovy
-
-import jenkins.model.*
 import hudson.security.*
+import jenkins.model.*
+import jenkins.security.*
 
 def instance = Jenkins.getInstance()
 
-println "--> creating local user 'admin'"
+if (!instance.isUseSecurity()) {
+    println "--> First launch: automatically creating local user 'admin'"
 
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount('{{ jenkins_admin.username }}','{{ jenkins_admin.password }}')
-instance.setSecurityRealm(hudsonRealm)
-
-def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-instance.setAuthorizationStrategy(strategy)
-instance.save()
+    def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+    hudsonRealm.createAccount('admin', 'admin')
+    instance.setSecurityRealm(hudsonRealm)
+    
+    def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+    instance.setAuthorizationStrategy(strategy)
+    instance.save()
+}
